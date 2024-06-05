68b978731e44b385a46b3b1b4b5ea2ea79a52a89
Lucas <>
2024-06-05 13:19:47 +0200

    Comment rotationUI

--- Assets/project/scripts/UILookAt.cs
+++ Assets/project/scripts/UILookAt.cs
@@ -5,20 +5,18 @@ using UnityEngine;
 
 public class UILookAt : MonoBehaviour
 {
-    XROrigin origin;
+    //XROrigin origin;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        origin = FindObjectOfType<XROrigin>();
-    }
+    //void Start()
+    //{
+    //    origin = FindObjectOfType<XROrigin>();
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
-        var oldRot = transform.rotation;
-        transform.LookAt(origin.transform);
-        oldRot = new Quaternion(oldRot.x, transform.rotation.y, oldRot.z, oldRot.w);
-        transform.rotation = oldRot;
-    }
+    //void Update()
+    //{
+    //    var oldRot = transform.rotation;
+    //    transform.LookAt(origin.transform);
+    //    oldRot = new Quaternion(oldRot.x, transform.rotation.y, oldRot.z, oldRot.w);
+    //    transform.rotation = oldRot;
+    //}
 }
